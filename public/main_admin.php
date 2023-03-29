fc3fb5c214cbe1e5112dfa0c4a86a12554570298
Dai Cat <phandaicat12032002@gmail.com>
2023-03-30 00:14:14 +0700

    secondary comment

--- public/main_admin.php
+++ public/main_admin.php
@@ -24,7 +24,7 @@
 <body>
     <!--begin HEADER -->
     <?php
-      include "../apps/resoures/view/partials/header_admin.php";
+      include "../apps/resoures/view/partials/header_admin.php ";
     ?>
     <!--end   HEADER -->
 
